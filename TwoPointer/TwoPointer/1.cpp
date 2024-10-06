8b6b2902d26b0acc63be02f3cbafc3930eb8fe88
taohidkhan22 <taohid130@gmail.com>
2024-10-06 13:32:13 +0600

    Hash table problem solved in optimized way & complexity reduced to n^2 to n

--- TwoPointer/TwoPointer/1.cpp
+++ TwoPointer/TwoPointer/1.cpp
@@ -1,43 +1,40 @@
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int ,int >mp;
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    v.push_back(i);
-                    v.push_back(j);
-                    break;
-
-                }
-
-            }
+            int a=nums[i], b=target-a;
+            if(mp.count(b)) return {mp[b],i};
+            else mp[a]=i;
         }
-        return v;
+        return {};
+
     }
 };
 int main()
-{ 
+{
     vector<int> a;
-     cout<<"Enter the size of the vector: ";
-    int n,k; cin>>n;
-    
-    cout<<"Enter the vector: ";
+    cout << "Enter the size of the vector: ";
+    int n, k;
+    cin >> n;
+
+    cout << "Enter the vector: ";
     for (int i = 0; i < n; i++)
     {
         int c;
-        cin>>c;
+        cin >> c;
         a.push_back(c);
     }
     Solution s;
-    cin>>k;
-    s.twoSum(a,k);
-
-  
-    
+    cin >> k;
+    vector<int> v = s.twoSum(a, k);
+    cout << "The output is: " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
 }
-
