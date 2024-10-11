6ba43a32ecb2afc53193ba95c2b95865f7bd01ad
taohidkhan22 <taohid130@gmail.com>
2024-10-11 23:10:23 +0600

    Tried the problem all the day but couldn't solve it, But i have learnt a lot. So, don't lose HOPE

--- HashMap/290.cpp
+++ HashMap/290.cpp
@@ -9,10 +9,47 @@ public:
 };
 int main()
 {
-    string s = {"i lov i lov"};
-    vector<string> v2;
-    map<int, string> mp;
-    string s1 = "";
+   
+    string s1 = "",p,f;
     int j = 0;
-     
+  
+    map<int, string> mp;
+    
+
+     getline(cin,f);
+       getline(cin,p);
+    map< string,int>pk;
+    map< char ,int>pk2;
+    for(int i=0;i<p.length();i++){
+        if(p[i]==' '){
+
+             pk.insert( pair< string,int>(s1,j));
+             s1.clear();
+             j++;
+
+        }
+        else{
+
+            s1+=p[i];
+        }
+       
+    } pk.insert( pair< string,int>(s1,j));
+
+    for(int i=0;i<f.size();i++){
+
+            pk2.insert( pair< char,int>(f[i],i));
+    }
+   for(auto i : pk){
+        cout<<i.first<<" - "<<i.second<<endl;
+
+       
+    }
+    
+
+    for(auto i : pk2){
+        cout<<i.first<<" - "<<i.second<<endl;
+
+       
+    }
+    
 }
