e989ad59961f34b758145b080c813639d9765937
taohidkhan22 <taohid130@gmail.com>
2024-11-02 20:19:38 +0600

    finding the character frequency in a word

---
+++ HashMap/characterFrequencyWord.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void makeFancyString(string s)
+    {
+
+        int hash[256] = {0};
+        for (int i = 0; i < s.size(); i++)
+        {
+            hash[s[i]]++;
+        }
+
+        for (int i = 0; i < 256; i++)
+        {
+            if (hash[i] > 0)
+            {
+                char a;
+                a = i;
+                cout << a << ".........." << hash[i] << endl;
+            }
+        }
+    }
+};
+int main()
+{
+    Solution sol;
+    string s;
+    getline(cin, s);
+    sol.makeFancyString(s);
+}
