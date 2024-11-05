8f8058bcc7bdef6fdbd744541f704f389c6a4a67
taohidkhan22 <taohid130@gmail.com>
2024-11-05 15:37:39 +0600

    BD phone number validation check using Regular Expression

--- tuitorial/emni/phoneNo.cpp
+++ tuitorial/emni/phoneNo.cpp
@@ -3,29 +3,12 @@ using namespace std;
 int main(){
     string s;
     getline(cin,s);
-    string s2="+8801";
-    for(int i=0;i<s2.size();i++){
-        if(s[i]!=s2[i]){
-           cout<<"1 Not bd number"<<endl;
-           break;
-        }
-    }
-    cout<<s2.size()<< " "<<s[s2.size()]<<endl;
-    if(s[s2.size()]=='7'||s[s2.size()]=='5'||s[s2.size()]=='9'||s[s2.size()]=='3'||s[s2.size()]=='8'){
-       
-    }
-    else{
-         cout<<"2 Not bd number"<<endl;
-    }
-    for(int i=s2.size()+1;i<s.size();i++){
-        if(s[i]>='0' && s[i]<='9'){
-           
-        }
-        else{
-           cout<<"3 Not bd number"<<endl;
-           break;
-        }
-    }
+   
+    regex pattern1(R"(^\+8801[3-9][1-9]{8}$)");
+   
+   
+    regex pattern2(R"(^01[3-9][1-9]{8}$)");
+    cout<<(regex_match(s, pattern1)||regex_match(s, pattern2))<<endl;
 
 
 
