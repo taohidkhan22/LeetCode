982b8a4bb0223c7a161df2040510efcdf1b70b3c
taohidkhan22 <taohid130@gmail.com>
2024-11-11 13:31:44 +0600

    Updated into tuitorial folder

--- tuitorial/BS/longestPrefix.cpp
+++ tuitorial/BS/longestPrefix.cpp
@@ -1,22 +1,7 @@
 // 387. First Unique Character in a String
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
-{
-public:
-    string longestCommonPrefix(vector<string> &strs)
-    {
-        sort(strs.begin(),strs.end());string s="";
-        string first=strs[0], last=strs[strs.size()-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]) break;
-            else {
-                s+=first[i];
-            }
-        }
-        return s;
-    }
-};
+;
 int main()
 {
     Solution S;
